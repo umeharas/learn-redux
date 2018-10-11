ef61a35f2038e80ceca754f34fcc1447ef081e62
umeharas <sumehara0605@gmail.com>
2018-10-11 14:56:11 +0900

    add build files

---
+++ my-app/build/precache-manifest.4d48b8aa2029f1bcd4133b5028369713.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4dc545835dc00e5e82fd",
+    "url": "/static/js/main.4dc54583.chunk.js"
+  },
+  {
+    "revision": "651133d331ee0cc69e07",
+    "url": "/static/js/1.651133d3.chunk.js"
+  },
+  {
+    "revision": "4dc545835dc00e5e82fd",
+    "url": "/static/css/main.99633645.chunk.css"
+  },
+  {
+    "revision": "48e62840ed7541c9c9556ddeb9290afe",
+    "url": "/index.html"
+  }
+];
